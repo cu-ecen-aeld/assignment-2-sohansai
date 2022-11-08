815a682a89f08ff898fe43d232fc1e4ce43a7c43
sohansai <76840110+sohansai@users.noreply.github.com>
2022-11-08 21:17:43 +0530

    Update writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,47 +1,71 @@
-#include <stdio.h>
-#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <string.h>
 #include <syslog.h>
 
-extern int errno;
+#define FILE_OPERATION_FAILURE -1
+#define OPERATION_FAILURE 1
 
-int main(int argc, char *argv[] )
+int cleanup(int status_code)
 {
-
-    openlog(NULL, 0, LOG_USER);
-    
-    if( argc != 3)
-    {
-        printf("Needs two params, for example './writer /tmp/aesd/assignment1 linux'");
-        syslog(LOG_ERR, "Invalid # of args: %d", argc);
-        return(1);
-    }
-
-    FILE *fp;
-    char buffer[255];
-    
-    fp = fopen(argv[1], "w+");
-    
-    if(fp == NULL)
-    {
-        printf("Could not open file");
-        syslog(LOG_ERR, "ERROR - Could not open file: %s", strerror(errno));
-        return(1);
-    }
-    else
-    {
-        //SYSLOG HERE
-        syslog(LOG_DEBUG, "Writing %s to %s", argv[2], argv[1]);
-        fprintf(fp, "%s", argv[2]);
-        fclose(fp);
-    }
-   
-    return 0;
-    
+	closelog();
+	return status_code;
 }
 
-//Setup syslog logging for your utility using the LOG_USER facility.
+int main(int argc, char** argv)
+{
+	// Setup syslog
+	// Have output to console if we cannot open log, and also write to stderr on an error.
+	openlog("Writer", LOG_CONS | LOG_PERROR,  LOG_USER);
 
-//Use the syslog capability to write a message “Writing <string> to <file>” where <string> is the text string written to file (second argument) and <file> is the file created by the script.  This should be written with LOG_DEBUG level.
+	// Check that we have enough arguments to properly run
+	if (argc < 2)
+	{
+		// Not enough arguments.
+		syslog(LOG_ERR, "Either a path was not provided, or no data string was supplied.\n");
+		syslog(LOG_ERR, "Usage: writer file_path data_string\n");
+		
+		return cleanup(OPERATION_FAILURE);
+	}
 
-//Use the syslog capability to log any unexpected errors with LOG_ERR level.
+	// Get the values for the target file name, and the data to write.
+	const char* file_path = argv[1];
+	const char* write_data = argv[2];
+
+	syslog(LOG_DEBUG, "Writing %s to %s\n", write_data, file_path);
+
+	// open the file in write mode. We are not reading the contents back,
+	// so there is no need to request read as well.
+	// We may also need to create the file.
+	// Truncate the file if it already exists.
+	int flags = O_WRONLY | O_CREAT | O_TRUNC;
+
+	// Set our mode if the file needs to be created
+	// to allow the owner to read and write, and everyone else
+	// to read.
+	int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	int file_descriptor = open(file_path, flags, mode);
+	if (file_descriptor == FILE_OPERATION_FAILURE)
+	{
+		// Although we are told the provided path WILL exist,
+		// we may not have write permissions to the file.
+		// Or, some other error happened.
+		syslog(LOG_ERR, "Failed to open the requested file.\n");
+		return cleanup(OPERATION_FAILURE);
+	}
+
+	int write_status = write(file_descriptor, write_data, strlen(write_data));
+	if (write_status < 0)
+	{
+		// An error must have happened during the write.
+		syslog(LOG_ERR, "Unable to write data to file.\n");
+		return OPERATION_FAILURE;
+	}
+
+	// Cleanup the logger and return status.
+
+	return cleanup(0);
+
+}
