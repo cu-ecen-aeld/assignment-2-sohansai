9e02e52a0b5810df4541ebf7e47a5dd53b336805
sohansai <76840110+sohansai@users.noreply.github.com>
2022-11-08 21:13:37 +0530

    Update finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,9 +8,10 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+OUTPUT_FILE=/tmp/assignment4-result.txt
+username=$(cat /etc/finder-app/conf/username.txt)
 
-if [ $# -lt 3 ]
+if [ $# -lt 2 ]
 then
 	echo "Using default value ${WRITESTR} for string to write"
 	if [ $# -lt 1 ]
@@ -32,37 +33,36 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
-
-if [ $assignment != 'assignment1' ]
-then
-	mkdir -p "$WRITEDIR"
+mkdir -p "$WRITEDIR"
 
 	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
 	#The quotes signify that the entire string in WRITEDIR is a single string.
 	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
+if [ -d "$WRITEDIR" ]
+then
+	echo "$WRITEDIR created"
+else
+	exit 1
 fi
-echo "Removing the old writer utility and compiling as a native application"
-make clean
-make
+# echo "Removing the old writer utility and compiling as a native application"
+#make clean
+#make
 
+echo "Writer starting!"
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+
+echo $OUTPUTSTRING > $OUTPUT_FILE
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
 set +e
+echo "Trying to match output"
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
 	echo "success"
